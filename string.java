e0f2d492915c58da6f73cd83b8591b149fb8ebe5
Aakriti negi <95736809+aakritinegi@users.noreply.github.com>
2022-04-17 13:01:27 +0530

    Update string.java

--- string.java
+++ string.java
@@ -1,4 +1,4 @@
-package Unit_01;
+package unit_01;
 
 /* Problem Statement:
 
@@ -25,7 +25,7 @@ public class P8_String {
 		String s2 = new String("7545a");
 
 		obj.palindromeOrNot(s1);
-		obj.reverseOfAString(s1);
+		obj.reverseOfAString(s2);
 		obj.stringEqualOrNot(s1, s2);
 
 	}
@@ -35,17 +35,48 @@ public class P8_String {
 class QuestionsOnString {
 
 	void palindromeOrNot(String s) {
-		// Write Logic Here!
+		int j = s.length()-1;
+		Boolean b = true;
+		for(int i = 0 ;i<=j; i++)
+		{
+			if(s.charAt(i)!= s.charAt(j))
+			{
+				b = false;
+				break;
+				
+			}j--;
+		}
+		if(b) {
+			System.out.println("string is pallindrome");
+		}
+		System.out.println("not pallindrome");
+		
+		
+		
+		
 
 	}
 
 	void reverseOfAString(String s) {
-		// Write Logic Here!
-
+		String st1=" ";
+		for(int i =0 ; i<s.length() ;i++)
+		{
+			char ch = s.charAt(i);
+			st1 = ch+st1;
+			
+		}System.out.println("reverse "+st1);
 	}
 	
+	
 	void stringEqualOrNot(String s1,String s2) {
-		// Write Logic Here!
-		
+		if(s1.equals(s2))
+		{
+			System.out.println("s1 = s2 ");
+		}
+		else {
+			System.out.println("s1 not equal to s2");
+		}
 	}
+		
+		
 }
